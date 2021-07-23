7c7dca3ee5b88435a67e9807461cf55a7d630253
Sahar Davidyan <Davidyan7@gmail.com>
2021-07-23 11:11:03 +0300

    dashboard

--- src/cmps/board/dashboard.vue
+++ src/cmps/board/dashboard.vue
@@ -3,12 +3,36 @@
     <button class="close-dashboard"></button>
     <h1>Dashboard</h1>
     <div class="charts-container">
-    <chart-bar class="bar-chart" :data="chartDataToDo" :options="chartOptions"/>
-    <chart-doughnut class="doughnut-chart"/>
-    <div>
-    <h1>Tasks per members</h1>
-    <chart-pie class="doughnut-chart" :data="chartDataMembers" :options="chartOptions"/>
-    </div>
+        <div>
+          <h2>Board details</h2>
+          <h2>Board title: {{selectedBoard.title}}</h2>
+          <h2>Members: {{selectedBoard.members.length}}</h2>
+          <h2>Creat at: {{selectedBoard.createdAt}}</h2>
+        </div>
+      <div>
+        <h2>Chacklist Todos</h2>
+        <chart-pie
+          class="pie-chart"
+          :data="chartDataToDo"
+          :options="chartOptions"
+        />
+      </div>
+      <div>
+      <h2>Tasks per group</h2>
+      <chart-bar
+        class="bar-chart"
+        :data="chartBarCardSum"
+        :options="chartOptions"
+      />
+      </div>
+      <div>
+        <h2>Tasks per members</h2>
+        <chart-pie
+          class="doughnut-chart"
+          :data="chartDataMembers"
+          :options="chartOptions"
+        />
+      </div>
     </div>
   </section>
 </template>
@@ -16,10 +40,10 @@
 <script>
 // import VueCharts from 'vue-chartjs'
 // import { Bar, Line } from 'vue-chartjs'
-import chartBar from './chart-bar.vue'
-import chartDoughnut from './chart-doughnut.vue'
-import chartPie from './chart-pie.vue'
-import {utilService} from '../../services/util-service.js'
+import chartBar from "./chart-bar.vue";
+import chartDoughnut from "./chart-doughnut.vue";
+import chartPie from "./chart-pie.vue";
+import { utilService } from "../../services/util-service.js";
 
 export default {
   data() {
@@ -28,85 +52,128 @@ export default {
         borderWidth: "10px",
         hoverBackgroundColor: "red",
         hoverBorderWidth: "10px",
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
       },
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+        "rgb(47, 145, 50)",
+        "rgb(205, 96, 0)",
+        "rgb(171, 0, 205)",
+      ],
     };
   },
   components: {
     chartBar,
     chartDoughnut,
-    chartPie
+    chartPie,
   },
   computed: {
     selectedBoard() {
       return JSON.parse(JSON.stringify(this.$store.getters.selectedBoard));
     },
-    chartDataToDo(){
-      let DoneToDo = 0
-      let undoneToDo = 0
-      const todos = []
-      this.selectedBoard.groups.forEach(group=>{
-        group.cards.forEach(card=>{
-          if(!card.checklists) return
+    chartBarCardSum() {
+      let groupTitels = [];
+      console.log("groupTitels", groupTitels.length)
+      let groupCards = [];
+      
+      this.selectedBoard.groups.forEach((group,groupIdx) => {
+        let sum = 0
+        groupTitels.push(group.title);
+        groupCards.push(sum)
+        group.cards.forEach((card, idx) => {
+          sum++
+           groupCards[groupIdx] = sum
+        });
+      });
+       return {
+        labels: groupTitels,
+        datasets: [
+          {
+            label: "Cards per group",
+            backgroundColor: ["rgb(54, 162, 235)","rgb(54, 162, 235)","rgb(54, 162, 235)","rgb(54, 162, 235)","rgb(54, 162, 235)","rgb(54, 162, 235)","rgb(54, 162, 235)","rgb(54, 162, 235)","rgb(54, 162, 235)"],
+            data: groupCards,
+          },
+        ],
+      };
+    },
+
+    chartDataToDo() {
+      let num = 0;
+      let DoneToDo = 0;
+      let undoneToDo = 0;
+      const todos = [];
+      this.selectedBoard.groups.forEach((group) => {
+        group.cards.forEach((card) => {
+          if (!card.checklists) return;
           card.checklists.forEach((checklist) => {
-            checklist.todos.forEach(todo=>{
-              if (todo.isDone ===true)DoneToDo++
-              else undoneToDo++
-            })
-      })
-        })
-      })
-      todos.push(DoneToDo)
-      todos.push(undoneToDo)
+            checklist.todos.forEach((todo) => {
+              if (todo.isDone === true) DoneToDo++;
+              else undoneToDo++;
+            });
+          });
+        });
+      });
+      todos.push(DoneToDo);
+      todos.push(undoneToDo);
       return {
-        labels: ['Done todo','undone todo'],
+        labels: ["Done todo", "undone todo"],
         datasets: [
           {
             label: "Checklist",
-            backgroundColor: todos.map(() => utilService.getRandomColor()),
+            backgroundColor: ["green", "rgb(255, 99, 132)"],
             data: todos,
           },
         ],
       };
-           
-      
     },
     chartDataMembers() {
       let membersCount = {};
-      this.selectedBoard.groups.forEach(group=>{
-        group.cards.forEach(card=>{
+      this.selectedBoard.groups.forEach((group) => {
+        group.cards.forEach((card) => {
           card.members.forEach((member) => {
-        if (membersCount[member.fullname]) {
-          membersCount[member.fullname] += 1;
-        } else {
-          membersCount[member.fullname] = 1;
-        }
-      })
-        })
-      })
-      console.log(membersCount);
+            if (membersCount[member.fullname]) {
+              membersCount[member.fullname] += 1;
+            } else {
+              membersCount[member.fullname] = 1;
+            }
+          });
+        });
+      });
       return {
         labels: Object.keys(membersCount),
         datasets: [
           {
             label: "Members",
-            backgroundColor: Object.keys(membersCount).map(() => utilService.getRandomColor()),
+            backgroundColor: Object.keys(membersCount).map(
+              (m, idx) => this.backgroundColor[idx]
+            ),
             data: Object.values(membersCount),
           },
         ],
       };
-    // chartData() {
-    //   return {
-    //     labels: this.selectedBoard.map((t) => 't.name'),
-    //     datasets: [
-    //       {
-    //         label: "Toys",
-    //         backgroundColor: this.selectedBoard.map(() =>
-    //           utilService.getRandomColor()
-    //         ),
-    //         data: this.selectedBoard.map((t) => 't.price'),
-    //       },
-    //     ],
-    //   };
+      // chartData() {
+      //   return {
+      //     labels: this.selectedBoard.map((t) => 't.name'),
+      //     datasets: [
+      //       {
+      //         label: "Toys",
+      //         backgroundColor: this.selectedBoard.map(() =>
+      //           utilService.getRandomColor()
+      //         ),
+      //         data: this.selectedBoard.map((t) => 't.price'),
+      //       },
+      //     ],
+      //   };
     },
   },
   methods: {
