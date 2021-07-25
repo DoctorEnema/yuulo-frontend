b461ff532151611983c513f17a908aaef8953f7b
Liran <liran450@gmail.com>
2021-07-25 22:09:55 +0300

    a

--- src/cmps/board/chart-doughnut.vue
+++ src/cmps/board/chart-doughnut.vue
@@ -4,22 +4,33 @@
 import { Doughnut } from "vue-chartjs";
 export default {
   extends: Doughnut,
+  props: { data: Object, options: Object },
   mounted() {
     // Overwriting base render method with actual data.
     this.renderChart({
-      labels: ["Red", "Blue", "Yellow"],
+      data:{
+        labels: ["Red", "Blue", "Yellow"],
       datasets: [
         {
           label: "My First Dataset",
           data: [300, 50, 100],
           backgroundColor: [
-            "rgb(255, 99, 132)",
+            "rgba(255, 99, 132, 0.5)",
             "rgb(54, 162, 235)",
             "rgb(255, 205, 86)",
           ],
           hoverOffset: 4,
         },
       ],
+      },
+      options: {
+        legend: {
+          display: false,
+          labels: {
+            fontSize: 0,
+          },
+        },
+      },
     });
   },
 };
